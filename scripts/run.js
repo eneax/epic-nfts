d798dff4f7275790c12b0775908fb13d354d0e4e
eneax <19818937+eneax@users.noreply.github.com>
2021-11-21 11:52:43 +0100

    Create run script

---
+++ scripts/run.js
@@ -0,0 +1,27 @@
+const main = async () => {
+  // Compile contract and generate required files stored under the `artifacts` directory
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+
+  // Hardhat creates local Ethereum network just for this contract
+  const nftContract = await nftContractFactory.deploy();
+
+  // Wait until contract is mined and deployed to local blockchain
+  await nftContract.deployed();
+
+  // `constructor` runs when we actually are fully deployed
+  // Contract address is printed on the console
+  // This address is how we can find our contract on the blockchain
+  console.log("Contract deployed to this address:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
